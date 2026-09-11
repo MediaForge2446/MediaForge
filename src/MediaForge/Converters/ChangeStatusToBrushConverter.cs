7a9fa63d9ae966618a9dbbb5ad6d4d64c15f1a6f
MediaForge2446 <popera541@gmail.com>
2026-09-11 16:25:57 +0300

    fix: bind dynamic status and media format state

--- src/MediaForge/Converters/ChangeStatusToBrushConverter.cs
+++ src/MediaForge/Converters/ChangeStatusToBrushConverter.cs
@@ -1,19 +1,28 @@
-using Microsoft.UI;
-using Microsoft.UI.Xaml.Data;
 using MediaForge.Core.Enums;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
 
 namespace MediaForge.Converters;
 
 public sealed class ChangeStatusToBrushConverter : IValueConverter
 {
+    private static readonly Brush SyncedBrush = new SolidColorBrush(Microsoft.UI.Colors.SeaGreen);
+    private static readonly Brush PendingBrush = new SolidColorBrush(Microsoft.UI.Colors.Goldenrod);
+    private static readonly Brush FailedBrush = new SolidColorBrush(Microsoft.UI.Colors.IndianRed);
+
     public object Convert(object value, Type targetType, object parameter, string language)
     {
-        return value switch
+        if (value is not ChangeStatus status)
         {
-            ChangeStatus.Synced => new Microsoft.UI.Xaml.Media.SolidColorBrush(Colors.ForestGreen),
-            ChangeStatus.Pending => new Microsoft.UI.Xaml.Media.SolidColorBrush(Colors.Goldenrod),
-            ChangeStatus.Failed => new Microsoft.UI.Xaml.Media.SolidColorBrush(Colors.Firebrick),
-            _ => new Microsoft.UI.Xaml.Media.SolidColorBrush(Colors.Gray)
+            return PendingBrush;
+        }
+
+        return status switch
+        {
+            ChangeStatus.Synced => SyncedBrush,
+            ChangeStatus.Pending => PendingBrush,
+            ChangeStatus.Failed => FailedBrush,
+            _ => PendingBrush
         };
     }
 
