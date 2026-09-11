7ff8a13aed4b42f928bbfdfe8541bae2048684e5
MediaForge2446 <popera541@gmail.com>
2026-09-11 16:56:33 +0300

    feat: add async filesystem enumeration

--- src/MediaForge.Core/Interfaces/IFileSystemService.cs
+++ src/MediaForge.Core/Interfaces/IFileSystemService.cs
@@ -5,6 +5,7 @@ namespace MediaForge.Core.Interfaces;
 public interface IFileSystemService
 {
     IReadOnlyList<FileItem> GetDirectoryItems(string directoryPath);
+    Task<IReadOnlyList<FileItem>> GetDirectoryItemsAsync(string directoryPath, CancellationToken cancellationToken = default);
     bool DirectoryExists(string directoryPath);
     bool FileExists(string filePath);
     void CreateDirectory(string directoryPath);
