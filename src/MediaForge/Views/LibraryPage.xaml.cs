a747b12a4a60faa883d47afadcf6cc71d2b0b593
MediaForge2446 <popera541@gmail.com>
2026-09-11 17:39:03 +0300

    fix: restore library page ViewModel namespace

--- src/MediaForge/Views/LibraryPage.xaml.cs
+++ src/MediaForge/Views/LibraryPage.xaml.cs
@@ -1,3 +1,4 @@
+using MediaForge.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Windows.Storage.Pickers;
@@ -8,25 +9,66 @@ namespace MediaForge;
 public sealed partial class LibraryPage : Page
 {
     public MainViewModel Model => ((MainWindow)App.MainWindow!).ViewModel;
-    public LibraryPage(){InitializeComponent(); DataContext=Model;}
+
+    public LibraryPage()
+    {
+        InitializeComponent();
+        DataContext = Model;
+    }
+
     private async void AddFolder(object sender, RoutedEventArgs e)
     {
         try
         {
-            var picker=new FolderPicker(); picker.FileTypeFilter.Add("*"); InitializeWithWindow.Initialize(picker, WindowNative.GetWindowHandle(App.MainWindow));
-            var folder=await picker.PickSingleFolderAsync(); if(folder is not null) Model.AddFolder(folder.Path);
+            var picker = new FolderPicker();
+            picker.FileTypeFilter.Add("*");
+            InitializeWithWindow.Initialize(picker, WindowNative.GetWindowHandle(App.MainWindow));
+            var folder = await picker.PickSingleFolderAsync();
+            if (folder is not null)
+            {
+                Model.AddFolder(folder.Path);
+            }
         }
-        catch(Exception ex){Model.ReportError(ex);}
+        catch (Exception ex)
+        {
+            Model.ReportError(ex);
+        }
     }
+
     private async void OpenFolder(object sender, ItemClickEventArgs e)
     {
-        if(e.ClickedItem is not LibraryFolder folder)return;
-        await Model.LoadFolderAsync(folder.Path, CancellationToken.None);
-        ((MainWindow)App.MainWindow!).NavigateToExplorer();
+        try
+        {
+            if (e.ClickedItem is not LibraryFolder folder)
+            {
+                return;
+            }
+
+            await Model.LoadFolderAsync(folder.Path, CancellationToken.None);
+            ((MainWindow)App.MainWindow!).NavigateToExplorer();
+        }
+        catch (Exception ex)
+        {
+            Model.ReportError(ex);
+        }
     }
+
     private async void AddMedia(object sender, RoutedEventArgs e)
     {
-        try { var dialog=new MediaDialog{XamlRoot=XamlRoot, DataContext=Model}; await dialog.ShowAsync(); Model.FilesRefresh(); }
-        catch(Exception ex){Model.ReportError(ex);}
+        try
+        {
+            var dialog = new MediaDialog
+            {
+                XamlRoot = XamlRoot,
+                DataContext = Model
+            };
+
+            await dialog.ShowAsync();
+            Model.FilesRefresh();
+        }
+        catch (Exception ex)
+        {
+            Model.ReportError(ex);
+        }
     }
 }
