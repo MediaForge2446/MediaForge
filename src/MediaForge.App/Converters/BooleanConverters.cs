b166e7aac65fa287d7fb12f36340fa37d55503dd
MediaForge2446 <popera541@gmail.com>
2026-09-13 23:49:54 +0300

    feat: add boolean visibility converter

--- src/MediaForge.App/Converters/BooleanConverters.cs
+++ src/MediaForge.App/Converters/BooleanConverters.cs
@@ -6,18 +6,27 @@ namespace MediaForge.App.Converters;
 
 public sealed class BooleanNotConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object value, Type targetType, object? parameter, CultureInfo culture)
         => value is bool boolean && !boolean;
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        => value is bool boolean && !boolean;
+    public object ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+        => value is bool boolean ? !boolean : Binding.DoNothing;
+}
+
+public sealed class BooleanToVisibilityConverterFallback : IValueConverter
+{
+    public object Convert(object value, Type targetType, object? parameter, CultureInfo culture)
+        => value is true ? Visibility.Visible : Visibility.Collapsed;
+
+    public object ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+        => value is Visibility visibility ? visibility == Visibility.Visible : Binding.DoNothing;
 }
 
 public sealed class InverseBooleanToVisibilityConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        => value is bool boolean && !boolean ? Visibility.Visible : Visibility.Collapsed;
+    public object Convert(object value, Type targetType, object? parameter, CultureInfo culture)
+        => value is false ? Visibility.Visible : Visibility.Collapsed;
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        => value is Visibility visibility && visibility != Visibility.Visible;
+    public object ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+        => value is Visibility visibility ? visibility != Visibility.Visible : Binding.DoNothing;
 }
