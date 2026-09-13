1347781ea461e00137bf02765c1de2ba446f56ce
MediaForge2446 <popera541@gmail.com>
2026-09-13 23:49:10 +0300

    feat: add downloads page code-behind

---
+++ src/MediaForge.App/Views/DownloadsPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MediaForge.App.Views;
+
+public partial class DownloadsPage : UserControl
+{
+    public DownloadsPage()
+    {
+        InitializeComponent();
+    }
+}
