e5c95b9fecb84dc74c478e769430b28e83dfcc4f
MediaForge2446 <popera541@gmail.com>
2026-09-13 23:48:39 +0300

    feat: add library page code-behind

---
+++ src/MediaForge.App/Views/LibraryPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MediaForge.App.Views;
+
+public partial class LibraryPage : UserControl
+{
+    public LibraryPage()
+    {
+        InitializeComponent();
+    }
+}
