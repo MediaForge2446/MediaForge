6e0c1ad7519c3364074434354ec647ee5f7fb0e7
MediaForge2446 <popera541@gmail.com>
2026-09-14 14:39:45 +0300

    feat: wire Explorer folder tree navigation

--- src/MediaForge.App/Views/ExplorerPage.xaml.cs
+++ src/MediaForge.App/Views/ExplorerPage.xaml.cs
@@ -1,9 +1,30 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MediaForge.App.ViewModels;
+
 namespace MediaForge.App.Views;
 
-public partial class ExplorerPage : System.Windows.Controls.UserControl
+public partial class ExplorerPage : UserControl
 {
     public ExplorerPage()
     {
         InitializeComponent();
+    }
+
+    private async void FolderTree_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+    {
+        if (DataContext is not ExplorerViewModel viewModel || e.NewValue is not ExplorerTreeNodeViewModel node)
+            return;
+
+        await viewModel.NavigateToPathAsync(node.FullPath);
+    }
+
+    private async void Entry_OnDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (DataContext is not ExplorerViewModel viewModel || sender is not ListViewItem item || item.Content is not ExplorerEntryViewModel entry)
+            return;
+
+        await viewModel.OpenFromDoubleClickAsync(entry);
     }
 }
