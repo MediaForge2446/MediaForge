5db2a411e359b008d8c119eb6a6fe9eb12fe1db6
MediaForge2446 <popera541@gmail.com>
2026-09-14 11:25:36 +0300

    feat: add navigation active-state converter

---
+++ src/MediaForge.App/Converters/StringEqualsConverter.cs
@@ -0,0 +1,13 @@
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MediaForge.App.Converters;
+
+public sealed class StringEqualsConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => string.Equals(value?.ToString(), parameter?.ToString(), StringComparison.OrdinalIgnoreCase) ? "Active" : null!;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => Binding.DoNothing;
+}
