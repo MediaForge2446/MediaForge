d501a04c706d5577a1ecb7ba9c4cc28b74afcf2b
MediaForge2446 <popera541@gmail.com>
2026-09-22 12:20:20 +0300

    refactor(ui): switch main window to FluentWindow

--- src/MediaForge.App/MainWindow.xaml.cs
+++ src/MediaForge.App/MainWindow.xaml.cs
@@ -1,13 +1,15 @@
 using System.Windows;
+using MediaForge.App.Localization;
 using MediaForge.App.ViewModels;
 
 namespace MediaForge.App;
 
-public partial class MainWindow : Window
+public partial class MainWindow : Wpf.Ui.Controls.FluentWindow
 {
     public MainWindow(MainViewModel viewModel)
     {
         InitializeComponent();
+        LocalizationService.Instance.ApplyToWindow(this);
         DataContext = viewModel;
     }
 }
