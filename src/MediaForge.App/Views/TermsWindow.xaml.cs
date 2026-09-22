7bb9cd43e89e9547c3c65fd32fe48f81f5e5a14e
MediaForge2446 <popera541@gmail.com>
2026-09-22 12:39:26 +0300

    fix(localization): apply runtime direction to terms window

--- src/MediaForge.App/Views/TermsWindow.xaml.cs
+++ src/MediaForge.App/Views/TermsWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using MediaForge.App.Localization;
 
 namespace MediaForge.App.Views;
 
@@ -7,6 +8,7 @@ public partial class TermsWindow : Window
     public TermsWindow()
     {
         InitializeComponent();
+        LocalizationService.Instance.ApplyToWindow(this);
     }
 
     private void ContinueButton_Click(object sender, RoutedEventArgs e)
