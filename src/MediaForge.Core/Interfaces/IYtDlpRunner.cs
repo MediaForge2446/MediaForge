0906b54b04ba8306d1f7335ee227f1fd755efa30
MediaForge2446 <popera541@gmail.com>
2026-09-24 10:51:14 +0300

    feat: pass explicit video quality through download contracts

--- src/MediaForge.Core/Interfaces/IYtDlpRunner.cs
+++ src/MediaForge.Core/Interfaces/IYtDlpRunner.cs
@@ -11,5 +11,6 @@ public interface IYtDlpRunner
         MediaFormat format,
         IProgress<DownloadProgress>? progress = null,
         CancellationToken cancellationToken = default,
-        MediaQuality quality = MediaQuality.Standard128K);
+        MediaQuality quality = MediaQuality.High192K,
+        MediaVideoQuality videoQuality = MediaVideoQuality.Balanced720p);
 }
